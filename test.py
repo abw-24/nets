72677b7d2b5908c4c975e70284365e483c0e3fea
awest-24 <awest.uchi@gmail.com>
2020-07-12 14:34:10 -0500

    slight updates to conv block, added regular and multi-path residual layers and blocks + a resnet class, but running into bugs

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ def smoke_decorator(fn):
     def _inner():
         try:
             fn()
-        except Exception() as e:
+        except BaseException() as e:
             print(e)
             return False
         else:
@@ -78,12 +78,8 @@ def basic_cnn():
         .batch(32)
 
     config = {
-        "filters": [16, 32],
-        "kernel": [3, 3],
-        "padding": ["same", "same"],
-        "conv_activation": "relu",
+        "conv_filters": [16, 32],
         "dense_dims": [32],
-        "dense_activation": "relu",
         "output_dim": 10,
         "output_activation": "softmax",
         "optimizer": {"Adam": {"learning_rate": 0.001}},
@@ -105,11 +101,55 @@ def basic_cnn():
         print("Epoch loss: {loss}".format(loss=loss/float(step)))
 
 
+@smoke_decorator
+def resnet():
+    """
+    Test mlp on mnist data.
+    """
+    import nets.nets as nets
+    import nets.train as t
+
+    # load mnist data, reshape to have channel dim, and normalize to 0-1
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+    x_train = x_train.reshape((x_train.shape[0], 28, 28, 1))
+    x_train = x_train / 255.0
+
+    # create a batch feed from the train tensors
+    train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)) \
+        .shuffle(10000) \
+        .batch(32)
+
+    config = {
+        "conv_filters": [16],
+        "res_filters": [[32, 32], [64, 64]],
+        "res_depth": 2,
+        "dense_dims": [32],
+        "output_dim": 10,
+        "output_activation": "softmax",
+        "optimizer": {"Adam": {"learning_rate": 0.001}},
+        "loss": {"SparseCategoricalCrossentropy": {}},
+        "epochs": 10
+    }
+
+    compiled_model = t.model_init(nets.ResNet(config), config, (None, 28, 28, 1))
+
+    for _ in range(config["epochs"]):
+        loss = 0.0
+        step = 0
+        for x, y in train_ds:
+            loss_, grads = t.grad(compiled_model, x, y)
+            updates = zip(grads, compiled_model.trainable_variables)
+            compiled_model.optimizer.apply_gradients(updates)
+            loss += loss_
+            step += 1
+        print("Epoch loss: {loss}".format(loss=loss/float(step)))
+
+
 if __name__ == "__main__":
 
     tf.keras.backend.set_floatx('float64')
 
-    tests = [mlp, basic_cnn]
+    tests = [resnet]
 
     for t in tests:
         status = "passed!" if t() else "failed!"
