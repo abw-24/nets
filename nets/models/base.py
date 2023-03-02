5d5247a50e420b26efa596bd3335dab789dcc024
awest-24 <awest.uchi@gmail.com>
2023-03-02 14:05:07 -0600

    added unit tests for dense layer, refactored variational encoding layer to inherit from tf.keras.Model (functions as a submodel)

--- nets/models/base.py
+++ nets/models/base.py
@@ -9,7 +9,7 @@ class BaseModel(tf.keras.Model, metaclass=ABCMeta):
     Abstract base class for `nets` models. Inherits from tf.keras.Model.
 
     The basic principle of the model implementations is to keep them
-    simple, retaining access/use of the high-level APIs whenever possible.
+    simple, retaining use of the high-level APIs whenever possible.
     To that end, the default approach is to override the `train_step` to
     control fitting, and to house tensor computations in tf.keras.Model
     subblocks if reasonable.
--- nets/models/vae.py
+++ nets/models/vae.py
@@ -5,8 +5,62 @@ VAE
 
 import tensorflow as tf
 
-from nets.layers.dense import DenseBlock, DenseGaussianVariationalEncoder
+from nets.layers.dense import DenseBlock
+from nets.layers.sampling import GaussianSampling
 from nets.models.base import BaseModel
+
+
+@tf.keras.utils.register_keras_serializable("nets")
+class DenseGaussianVariationalEncoder(tf.keras.Model):
+
+    def __init__(self, encoding_dims, latent_dim, activation="relu",
+                 activity_regularizer=None, sparse_flag=False, name="VAE",
+                 **kwargs):
+
+        super().__init__(name=name, **kwargs)
+
+        self._encoding_dims = encoding_dims
+        self._latent_dim = latent_dim
+        self._activation = activation
+        self._a_regularizer = activity_regularizer
+        self._sparse_flag = sparse_flag
+
+        self._encoding_block = DenseBlock(
+                hidden=self._encoding_dims,
+                activation=self._activation,
+                activity_regularizer=self._activity_regularizer
+        )
+        self._latent_mean = tf.keras.layers.Dense(self._latent_dim)
+        self._latent_log_var = tf.keras.layers.Dense(self._latent_dim)
+        self._sampling = GaussianSampling()
+
+    def call(self, inputs, training=False):
+        """
+
+        :param inputs:
+        :return:
+        """
+        x = self._encoding_block(inputs)
+        z_mean = self._latent_mean(x)
+        z_log_var = self._latent_log_var(x)
+        z = self._sampling((z_mean, z_log_var))
+        return z_mean, z_log_var, z
+
+    def get_config(self):
+        config = super(DenseGaussianVariationalEncoder, self).get_config()
+        config.update({
+            "encoding_dims": self._encoding_dims,
+            "latent_dim": self._latent_dim,
+            "input_dim": self._input_shape,
+            "activation": self._activation,
+            "activity_regularizer": self._activity_regularizer,
+            "sparse_flag": self._sparse_flag
+        })
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
 
 
 @tf.keras.utils.register_keras_serializable("nets")
