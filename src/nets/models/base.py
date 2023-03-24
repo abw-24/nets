e32eaaee8476ff98b1652ab0b650b8daa3dfb616
awest-24 <awest.uchi@gmail.com>
2023-03-24 13:52:34 -0500

    updating model ABC(s) -- separate ABCs for models inheriting from tf.keras.Model and tfs.models.Model respectively

--- src/nets/models/base.py
+++ src/nets/models/base.py
@@ -1,12 +1,13 @@
 
 from abc import ABCMeta
 import tensorflow as tf
+import tensorflow_recommenders as tfrs
 
 
 @tf.keras.utils.register_keras_serializable("nets")
-class BaseModel(tf.keras.Model, metaclass=ABCMeta):
+class BaseTFKerasModel(tf.keras.Model, metaclass=ABCMeta):
     """
-    Abstract base class for `nets` models. Inherits from tf.keras.Model.
+    Abstract base class for models inheriting from tf.keras.Model.
 
     The basic principle of the model implementations is to keep them
     simple, retaining use of the high-level APIs whenever possible.
@@ -22,4 +23,23 @@ class BaseModel(tf.keras.Model, metaclass=ABCMeta):
         :param kwargs: Keras model keyword arguments
         :return: None
         """
-        super(BaseModel, self).__init__(name=name, **kwargs)
+        super().__init__(name=name, **kwargs)
+
+
+@tf.keras.utils.register_keras_serializable("nets")
+class BaseTFRecommenderModel(tfrs.models.Model, metaclass=ABCMeta):
+    """
+    Abstract base class for models inheriting from tfrs.models.Model.
+
+    The basic principle of the model implementations is to keep them
+    simple, retaining use of the high-level APIs whenever possible.
+    """
+
+    def __init__(self, name: str, **kwargs):
+        """
+
+        :param name: Model name (str)
+        :param kwargs: Recommender keyword arguments
+        :return: None
+        """
+        super().__init__(name=name, **kwargs)
