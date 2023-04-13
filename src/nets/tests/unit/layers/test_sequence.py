49dd9423fddf377e3c027e217e555e934f83ebcb
awest-24 <awest.uchi@gmail.com>
2023-04-13 08:07:16 -0500

    adding masking constructor option to MHA testing

--- src/nets/tests/unit/layers/test_sequence.py
+++ src/nets/tests/unit/layers/test_sequence.py
@@ -2,11 +2,11 @@
 import unittest
 import tensorflow as tf
 
-from nets.layers.sequence import MultiHeadMaskedSelfAttention as MHA
+from nets.layers.sequence import MultiHeadSelfAttention as MHSA
 from nets.tests.utils import try_except_assertion_decorator
 
 
-class TestMultiHeadMaskedSelfAttention(unittest.TestCase):
+class TestMultiHeadSelfAttention(unittest.TestCase):
 
     def setUp(self):
         """
@@ -15,29 +15,32 @@ class TestMultiHeadMaskedSelfAttention(unittest.TestCase):
         self._input_shape = (16, 10, 256)
         self._num_heads = 2
         self._key_dim = 128
+        self._masking = True
 
         self._default_config = {
             "num_heads": self._num_heads,
-            "key_dim": self._key_dim
+            "key_dim": self._key_dim,
+            "masking": self._masking
         }
 
     @try_except_assertion_decorator
     def test_build_basic_with_constructor_defaults(self):
-        _ = MHA()
+        _ = MHSA()
 
     @try_except_assertion_decorator
     def test_build_basic(self):
-        _ = MHA(
+        _ = MHSA(
                 num_heads=self._num_heads,
-                key_dim=self._key_dim
+                key_dim=self._key_dim,
+                masking=self._masking
         )
 
     @try_except_assertion_decorator
     def test_build_from_config(self):
-        _ = MHA.from_config(self._default_config)
+        _ = MHSA.from_config(self._default_config)
 
     def test_build_get_config(self):
-        block = MHA(**self._default_config)
+        block = MHSA(**self._default_config)
         c = block.get_config()
         assert isinstance(c, dict)
         assert all([k in c for k, v in self._default_config.items()]), \
@@ -49,7 +52,7 @@ class TestMultiHeadMaskedSelfAttention(unittest.TestCase):
 
     def test_call(self):
         inputs = tf.random.normal(self._input_shape)
-        block = MHA(**self._default_config)
+        block = MHSA(**self._default_config)
         output = block(inputs)
         assert output.shape == self._input_shape, \
             "MHA input shape: {}\nMHA output shape: {}".format(
