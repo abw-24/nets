7cf066240a2706d618840b23806afacd6de8f426
awest-24 <awest.uchi@gmail.com>
2023-04-13 10:23:09 -0500

    adding concatenation option for MHSA layer

--- src/nets/layers/sequence.py
+++ src/nets/layers/sequence.py
@@ -6,12 +6,16 @@ import tensorflow as tf
 class MultiHeadSelfAttention(tf.keras.layers.Layer):
     """
     Self-attention with residual connections, as in Transformer setups.
-    Optionally can set `masking` to True for "causal" training with a
-    1-step shifted target vector, as in the original paper.
+
+    - If `masking` is set to True, the attention layer is called with the
+    `use_causal_mask` arg set to True.
+    - If `pooling` is set to True, the MHA output is averaged over the
+    timestep dimension.
+    - If `concat` is set to True, the MHA timesteps are concatenated.
     """
 
     def __init__(self, num_heads=4, key_dim=128, masking=False,
-                 pooling=False, name="MultiHeadSelfAttention"):
+                 pooling=False, concat=False, name="MultiHeadSelfAttention"):
 
         super().__init__(name=name)
 
@@ -19,32 +23,41 @@ class MultiHeadSelfAttention(tf.keras.layers.Layer):
         self._key_dim = key_dim
         self._masking = masking
         self._pooling = pooling
+        self._concat = concat
 
         self._pooling_tensor_flag = tf.convert_to_tensor(
                 self._pooling, dtype=tf.bool
         )
-        self._attention = tf.keras.layers.MultiHeadAttention(
+        self._concat_tensor_flag = tf.convert_to_tensor(
+                self._concat, dtype=tf.bool
+        )
+        self._attention_layer = tf.keras.layers.MultiHeadAttention(
                 num_heads=num_heads, key_dim=key_dim
         )
-        self._add = tf.keras.layers.Add()
+        self._add_layer = tf.keras.layers.Add()
         self._layer_norm = tf.keras.layers.LayerNormalization()
 
         if self._pooling_tensor_flag:
             self._global_pool = tf.keras.layers.GlobalAveragePooling1D()
 
+        if self._concat_tensor_flag:
+            self._concat_layer = tf.keras.layers.Concatenate()
+
     def build(self, input_shape):
         super().build(input_shape)
 
     def call(self, inputs):
-        output = self._attention.__call__(
+        output = self._attention_layer.__call__(
                 query=inputs,
                 value=inputs,
                 key=inputs,
                 use_causal_mask=self._masking
         )
-        embedding = self._add.__call__([inputs, output])
+        embedding = self._add_layer.__call__([inputs, output])
         if self._pooling_tensor_flag:
             embedding = self._global_pool.__call__(embedding)
+        elif self._concat_tensor_flag:
+            embedding = self._concat_layer(embedding, axis=2)
         embedding = self._layer_norm.__call__(embedding)
         return embedding
 
@@ -53,6 +66,8 @@ class MultiHeadSelfAttention(tf.keras.layers.Layer):
         config.update({
             "num_heads": self._num_heads,
             "key_dim": self._key_dim,
-            "masking": self._masking
+            "masking": self._masking,
+            "pooling": self._pooling,
+            "concat": self._concat
         })
         return config
