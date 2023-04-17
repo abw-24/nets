5a370a4c19fb82030e70b7401a40906c4d1b6a4d
awest-24 <awest.uchi@gmail.com>
2023-04-17 10:57:53 -0500

    general util moved to tests utils (should only be used in the context of testing)

--- src/nets/tests/utils.py
+++ src/nets/tests/utils.py
@@ -47,3 +47,26 @@ def try_except_assertion_decorator(fn):
         assert success, msg
 
     return wrapper
+
+
+def get_obj(module, obj_dict=None):
+    """
+    Create a class instance from a given module with keyword args.
+    Intended to aid JSON model configuration.
+
+    Expects obj_dict to take the following form:
+        {"Name": {"kwarg": kwargval, "kwarg2": kwargval}}
+
+    Example:
+        get_obj(tf.keras.optimizers, {"SGD": {"learning_rate": 0.001,
+            "momentum": 0.01})
+
+    :param module: Module
+    :param obj_dict: Dict
+    :return: Instance
+    """
+    if obj_dict is None:
+        return None
+    else:
+        obj, kwargs = list(obj_dict.items())[0]
+        return getattr(module, obj)(**kwargs)
