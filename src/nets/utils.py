feb725a5d8f245f573fe0d20e60b923d55a1b260
awest-24 <awest.uchi@gmail.com>
2023-04-17 10:37:06 -0500

    comments

--- src/nets/utils.py
+++ src/nets/utils.py
@@ -2,19 +2,19 @@
 
 def get_obj(module, obj_dict=None):
     """
-    Instantiate an object from a given module with keyword args.
+    Create a class instance from a given module with keyword args.
     Intended to aid JSON / string model configuration.
 
     Expects obj_dict to take the following form:
-        {"ObjectName": {"kwarg": kwargval, "kwarg2": kwargval}}
+        {"Name": {"kwarg": kwargval, "kwarg2": kwargval}}
 
     Example:
-        get_tf(tf.keras.optimizers, {"SGD": {"learning_rate": 0.001,
+        get_obj(tf.keras.optimizers, {"SGD": {"learning_rate": 0.001,
             "momentum": 0.01})
 
     :param module: Module
     :param obj_dict: Dict
-    :return: Instance of object
+    :return: Instance
     """
     if obj_dict is None:
         return None
