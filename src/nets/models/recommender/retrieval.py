4f8bdc4d1056b97ade8a88cf718988d7f94f8bc9
awest-24 <awest.uchi@gmail.com>
2023-04-19 11:59:57 -0500

    adding mhsa mixture of experts retrieval model

--- src/nets/models/recommender/retrieval.py
+++ src/nets/models/recommender/retrieval.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 import tensorflow_recommenders as tfrs
 
 from .base import TwoTowerABC, TwoTowerTrait
+from nets.models.recommender.embedding import HashEmbedding, DeepHashEmbedding
+from nets.layers.dense import GatedMixture
 
 
 @tf.keras.utils.register_keras_serializable("nets")
@@ -49,3 +51,76 @@ class TwoTowerRetrieval(TwoTowerTrait, TwoTowerABC):
                 compute_metrics=not training,
                 compute_batch_metrics=not training
         )
+
+
+class MixtureOfAttentionExpertsRetrieval(TwoTowerRetrieval):
+
+    """
+    Sequential mixture of experts (MoE). Experts are two multi-head attention
+    networks with
+    """
+
+    def __init__(self, query_id, candidate_id, share_embeddings=True,
+                 embedding_dim=32, attention_pooling=True,
+                 query_context_features=None, candidate_context_features=None,
+                 name="SelfAttentionMixtureOfExpertsRetrieval"):
+
+        self._share_embeddings = share_embeddings
+        self._embedding_dim = embedding_dim
+        self._attention_pooling = attention_pooling
+        self._hash_embeddings_dim = 128
+
+        # If the experts share embeddings, create them
+        shared_embeddings = None
+        if self._share_embeddings:
+            shared_embeddings = HashEmbedding(
+                embedding_dim=self._hash_embeddings_dim
+            )
+
+        # Long range model has more heads, larger attention dim
+        long_range_model = DeepHashEmbedding(
+                embeddings=shared_embeddings,
+                hash_embedding_dim=self._hash_embeddings_dim,
+                embedding_dim=self._embedding_dim,
+                hidden_dims=[64],
+                attention_key_dim=128,
+                attention_heads=4,
+                attention_pooling=self._attention_pooling
+        )
+
+        # Short range model with just 1 head, small attention dim
+        short_range_model = DeepHashEmbedding(
+                embeddings=shared_embeddings,
+                hash_embedding_dim=self._hash_embeddings_dim,
+                embedding_dim=self._embedding_dim,
+                hidden_dims=[64],
+                attention_key_dim=64,
+                attention_heads=1,
+                attention_pooling=self._attention_pooling
+        )
+
+        query_model = GatedMixture(
+            experts=[long_range_model, short_range_model],
+            expert_dim=self._embedding_dim
+        )
+
+        candidate_model = DeepHashEmbedding(
+                hash_embedding_dim=self._hash_embeddings_dim,
+                hidden_dims=[64],
+                embedding_dim=self._embedding_dim
+        )
+
+        super().__init__(
+                query_model=query_model,
+                candidate_model=candidate_model,
+                query_id=query_id,
+                candidate_id=candidate_id,
+                query_context_features=query_context_features,
+                candidate_context_features=candidate_context_features,
+                name=name
+        )
+
+    def build(self, input_shape):
+        # Make sure to build the gated mixture of experts model
+        self._query_model.build(input_shape=input_shape)
+        super().build(input_shape=input_shape)
