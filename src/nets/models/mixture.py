2b60837c99201e3da17cb2d91fa5c075f8edf883
awest-24 <awest.uchi@gmail.com>
2023-04-20 16:48:19 -0500

    mixture model switched to a more trait/abc style, where experts are expected to be defined by inheritors

--- src/nets/models/mixture.py
+++ src/nets/models/mixture.py
@@ -2,33 +2,36 @@
 import tensorflow as tf
 import tensorflow_addons as tfa
 
+from nets.models.base import BaseTFKerasModel
+
 
 @tf.keras.utils.register_keras_serializable("nets")
-class GatedMixture(tf.keras.Model):
+class GatedMixtureABC(BaseTFKerasModel):
     """
     Gated mixture of experts.
 
     Takes raw inputs, passes them through each expert and gate
     layer pair, elementwise multiplies the pairs, and sums,
-    returning a tensor of shape (batch_size, embedding_dim)
+    returning a tensor of shape (batch_size, embedding_dim).
+
+    Inheritors should define the expert models in the init method and
+     assign a list of the experts to a `self._experts` instance variable.
     """
 
-    def __init__(self, experts, embedding_dim, spectral_norm=False,
-                 name="GatedMixture", **kwargs):
+    def __init__(self, n_experts, expert_dim, spectral_norm=False,
+                 name="GatedMixtureABC", **kwargs):
 
         super().__init__(name=name, **kwargs)
 
-        self._experts = experts
-        self._n_experts = len(experts)
-        self._expert_dim = embedding_dim
+        self._n_experts = n_experts
+        self._expert_dim = expert_dim
         self._spectral_norm = spectral_norm
-        self._batch_size = None
 
         self._gate_layers = []
 
         for i in range(self._n_experts):
             dense_layer = tf.keras.layers.Dense(
-                    units=embedding_dim, activaton="sigmoid"
+                    units=expert_dim, activation="sigmoid"
             )
             if self._spectral_norm:
                 dense_layer = tfa.layers.SpectralNormalization(
@@ -36,16 +39,9 @@ class GatedMixture(tf.keras.Model):
                 )
             self._gate_layers.append(dense_layer)
 
-        self._add = tf.keras.layers.Add()
-
-    def build(self, input_shape):
-        self._batch_size = input_shape[0]
-        super().build(input_shape)
-
     def call(self, inputs, training=True):
 
-        # Create an empty tensor to add to
-        outputs = tf.zeros(shape=(self._batch_size, self._expert_dim))
+        outputs = None
 
         # Iterate over experts and gate layers, take the elementwise product
         # and add to the outputs sum vector.
@@ -54,14 +50,22 @@ class GatedMixture(tf.keras.Model):
             expert_output = expert.__call__(inputs)
             gates = gate_layer.__call__(inputs)
             gated_expert = tf.math.multiply(gates, expert_output)
-            outputs = self._add.__call__([outputs, gated_expert])
+            if outputs is None:
+                outputs = gated_expert
+            else:
+                outputs = tf.math.add(outputs, gated_expert)
 
         return outputs
 
     def get_config(self):
         config = super().get_config()
         config.update({
-            "experts": self._experts,
-            "embedding_dim": self._embedding_dim
+            "n_experts": self._n_experts,
+            "expert_dim": self._expert_dim,
+            "spectral_norm": self._spectral_norm
         })
         return config
+
+    @property
+    def experts(self):
+        return self._experts
