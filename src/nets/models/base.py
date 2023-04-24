0b8343c74dc3bc53aef620a06165d7860c2c3e15
awest-24 <awest.uchi@gmail.com>
2023-04-23 20:13:48 -0500

    updated base ABCs to include abstract methods

--- src/nets/models/base.py
+++ src/nets/models/base.py
@@ -1,5 +1,5 @@
 
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
 import tensorflow as tf
 import tensorflow_recommenders as tfrs
 
@@ -9,11 +9,11 @@ class BaseTFKerasModel(tf.keras.Model, metaclass=ABCMeta):
     """
     Abstract base class for models inheriting from tf.keras.Model.
 
-    The basic principle of the model implementations is to keep them
+    The basic principle of the tf.keras model implementations is to keep them
     simple, retaining use of the high-level APIs whenever possible.
     To that end, the default approach is to override the `train_step` to
-    control fitting, and to house tensor computations in tf.keras.Model
-    subblocks if reasonable.
+    control fitting, and to house tensor computations in registered
+    tf.keras.Model subblocks and/or registered custom layers.
     """
 
     def __init__(self, name: str, **kwargs):
@@ -25,6 +25,14 @@ class BaseTFKerasModel(tf.keras.Model, metaclass=ABCMeta):
         """
         super().__init__(name=name, **kwargs)
 
+    @abstractmethod
+    def call(self, inputs, training=False):
+        raise NotImplementedError("Abstract.")
+
+    @abstractmethod
+    def get_config(self, inputs, training=False):
+        raise NotImplementedError("Abstract.")
+
 
 @tf.keras.utils.register_keras_serializable("nets")
 class BaseTFRecommenderModel(tfrs.models.Model, metaclass=ABCMeta):
@@ -33,6 +41,12 @@ class BaseTFRecommenderModel(tfrs.models.Model, metaclass=ABCMeta):
 
     The basic principle of the model implementations is to keep them
     simple, retaining use of the high-level APIs whenever possible.
+
+    Generally speaking, only submodels are of interest with recommenders (for
+    post-training serialization), so recommender model definitions often
+    focus on `compute_loss`. If for some reason the top-level model needs to
+    be serialized as a whole for later inference, inherit from BaseTFKerasModel
+    and follow that paradigm instead.
     """
 
     def __init__(self, name: str, **kwargs):
@@ -43,3 +57,11 @@ class BaseTFRecommenderModel(tfrs.models.Model, metaclass=ABCMeta):
         :return: None
         """
         super().__init__(name=name, **kwargs)
+
+    @abstractmethod
+    def call(self, inputs, training=False):
+        raise NotImplementedError("Abstract.")
+
+    @abstractmethod
+    def compute_loss(self, features, training=False):
+        raise NotImplementedError("Abstract")
