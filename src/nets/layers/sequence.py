relative positional encoding update (broadcasting to input dim), fixes

src/nets/layers/sequence.py
@@ -118,15 +118,6 @@ class PositionEncoding(tf.keras.layers.Layer):
         self._seq_axis = seq_axis
 
         self._position_embeddings = None
-        self._embedded_dim = None
-
-    def get_config(self):
-        config = super().get_config()
-        config.update({
-            "max_length": self._max_length,
-            "seq_axis": self._seq_axis,
-        })
-        return config
 
     def build(self, input_shape):
 
@@ -141,8 +132,8 @@ class PositionEncoding(tf.keras.layers.Layer):
 
     def call(self, inputs):
         """
-        Grab embeddings from lookup for positions present in the batch input
-        (0 through size of `seq_axis`), and broadcast to full input size
+        Grab embeddings from the lookup for indices based on the shape of the
+        input (the size of the `seq_dim`)
         """
         input_shape = tf.shape(inputs)
 
@@ -150,20 +141,22 @@ class PositionEncoding(tf.keras.layers.Layer):
         seq_len = input_shape[self._seq_axis]
         position_embeddings = self._position_embeddings[:seq_len, :]
 
-        # Create a new shape placeholder (1, seq_len, embedding_dim)
-        new_shape = [1 for _ in inputs.get_shape().as_list()]
-        new_shape[self._seq_axis] = seq_len
-        new_shape[-1] = self._embedded_dim
+        # Expand dims and broadcast to full (batch, seq_len, embedding_dim)
+        position_embeddings = tf.expand_dims(position_embeddings, axis=0)
 
-        # Reshape embeddings to (1, seq_len, embedding_dim)
-        position_embeddings = tf.reshape(position_embeddings, new_shape)
-
-        # Broadcast to full (batch, seq_len, embedding_dim)
         return tf.broadcast_to(position_embeddings, input_shape)
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            "max_length": self._max_length,
+            "seq_axis": self._seq_axis,
+        })
+        return config
 
 
 @tf.keras.utils.register_keras_serializable("nets")
-class RelativePositionEmbedding(tf.keras.layers.Layer):
+class RelativePositionEncoding(tf.keras.layers.Layer):
     """
     Attention-is-all-you-need style positional encodings using trigonometric
       functions. Does not require a maxium length.
@@ -171,8 +164,8 @@ class RelativePositionEmbedding(tf.keras.layers.Layer):
       Based on the `tfm` implementation.
     """
 
-    def __init__(self, hidden_size, min_timescale=1.0,
-                 max_timescale = 1.0e4, **kwargs):
+    def __init__(self, hidden_dim, min_timescale=1.0,
+                 max_timescale=1.0e4, **kwargs):
 
         # Unless otherwise specified, default to float32
         if "dtype" not in kwargs:
@@ -180,7 +173,7 @@ class RelativePositionEmbedding(tf.keras.layers.Layer):
 
         super().__init__(**kwargs)
 
-        self._hidden_size = hidden_size
+        self._hidden_dim = hidden_dim
         self._min_timescale = min_timescale
         self._max_timescale = max_timescale
 
@@ -189,7 +182,7 @@ class RelativePositionEmbedding(tf.keras.layers.Layer):
         input_shape = tf.shape(inputs)
         length = input_shape[1]
         position = tf.cast(tf.range(length), tf.float32)
-        num_timescales = self._hidden_size // 2
+        num_timescales = self._hidden_dim // 2
 
         min_timescale, max_timescale = self._min_timescale, self._max_timescale
 
@@ -207,12 +200,16 @@ class RelativePositionEmbedding(tf.keras.layers.Layer):
         position_embeddings = tf.concat(
                 [tf.sin(scaled_time), tf.cos(scaled_time)], 1
         )
-        return position_embeddings
+
+        # Expand dims and broadcast position embeddings back to input_shape
+        position_embeddings = tf.expand_dims(position_embeddings, axis=0)
+
+        return tf.broadcast_to(position_embeddings, input_shape)
 
     def get_config(self):
         config = super().get_config()
         config.update({
-            "hidden_size": self._hidden_size,
+            "hidden_dim": self._hidden_dim,
             "min_timescale": self._min_timescale,
             "max_timescale": self._max_timescale,
         })